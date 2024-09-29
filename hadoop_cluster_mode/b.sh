b3f3aa753043b2df21eca43e3df8f7471e496aaf
raul <raul_fernando_mendoza@hotmail.com>
2024-09-28 20:06:00 -0600

    using make file

--- hadoop_cluster_mode/b.sh
+++
@@ -1,7 +0,0 @@
-docker stop $(docker ps -aq)
-docker rm $(docker ps -aq)
-docker-compose up --build -d
-rm -f ./public_keys_all/*
-. keys_get.sh
-docker exec -it namenode /bin/bash -c ./namenode-setup.sh
-
